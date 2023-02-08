dbbadc1881845abaf68f037edd37b0fa2b73f25f
Joel Torres <jotozhun@espol.edu.ec>
2023-02-07 21:00:29 -0500

    Completed scrapping and added tests

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # RPA_Challenge_NY
+This project scraps the new york times website which is https://nytimes.com to get data of the news, apply filters to search specific news.
+## Requirements
+- python version 3.10.5
+## First steps
+> python -m venv venv
+
+### For Linux
+> source ./venv/bin/activate
+
+### For Windows
+> .\venv\Scripts\activate
+
+### Then
+> pip install --upgrade pip
+
+> pip install -r requirements.txt
+
+## Configuration
+In the **main.py** file you have three variables.
+
+**words** phrase that will be use to search
+
+**categories** is a list of the sections to filter, the available options are: *Any, Arts, Books, Magazine, Movies, New York, Opinion, Theater, U.S., World*
+
+**number_of_months** the number of months to search the news. 0 - 1 is for news of the current month, 2 for news since the last month and so on and so forth.
+
+## Run
+
+To execute the script
+> python main.py
+
+To run tests
+> python -m unittest ./tests/test_dollar_pattern.py
--- main.py
+++ main.py
@@ -1,85 +1,120 @@
 import time
 import requests
+import pandas
+import os
 
-from objects.NYNEWS_OBJ import NYNEWS_OBJ
-from utils.utils import map_categories_to_index
+from models.NYNEWS_OBJ import NYNEWS_OBJ
+from utils.utils import map_categories_to_index, normalize_months_to_watch, find_dollar_ocurrence
 from utils.nytimes_vars import *
-from datetime import date, datetime
+from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from SeleniumLibrary.errors import ElementNotFound
 
-nynews_obj = NYNEWS_OBJ(url=NYTIMES_URL)
-word = "murder"
-categories = ["Arts", "U.S.", "World"]
 
-today_date = datetime.now()
-print(today_date.strftime(EXECUTION_NAME_FORMAT))
+def main(word: str, categories: list[str], number_of_months: int):
+    # Lists of lists with the information of earch row
+    df_rows_list = []
 
-categories_indexes = map_categories_to_index(categories, CATEGORIES_MAP_INDEX)
+    nynews_obj = NYNEWS_OBJ(url=NYTIMES_URL)
+    today_date = datetime.now()
+    destination_folder = f"logs/{today_date.strftime(EXECUTION_NAME_FORMAT)}"
+    categories_indexes = map_categories_to_index(categories, CATEGORIES_MAP_INDEX)
 
-# Define a main() function that calls the other functions in order:
-def main():
     try:
+        # Search a word
         nynews_obj.click_on_btn()
         nynews_obj.search_for(word=word)
-        # time.sleep(5)
+        nynews_obj.click_on_element(SORT_FILTER_BTN)
+        nynews_obj.press_key(SORT_FILTER_BTN, DOWN_ARROW)
 
-        nynews_obj.click_on_element("css:select.css-v7it2b")
-        nynews_obj.press_key("css:select.css-v7it2b", "DOWN")
-        # time.sleep(5)
-        nynews_obj.click_on_btn("css:button.css-4d08fs")
-        nynews_obj.wait_for_element_to_be_present("css:div.css-tw4vmx")
-        # time.sleep(1)
-        # parent_div = nynews_obj.browser_lib.find_element("css:div.css-tw4vmx")
-        category_elements = nynews_obj.browser_lib.find_elements('css:input[type="checkbox"]')
+        # Filter for most recent news
+        nynews_obj.click_on_btn(SECTION_FILTER_BTN)
+        nynews_obj.wait_for_element_to_be_present(SECTION_FILTER_DROPDOWN)
+
+        # Select the categories for the filter
+        category_elements = nynews_obj.browser_lib.find_elements(SECTION_CHECKBOX)
         for category_index in categories_indexes:
             category_elements[category_index].click()
-        nynews_obj.click_on_btn("css:button.popup-visible.css-4d08fs")
+        nynews_obj.click_on_btn(HIDE_SECTION_FILTER_BTN)
 
+        # Filter for date ranges
+        nynews_obj.click_on_btn(DATE_FILTER_BTN)
+        nynews_obj.wait_for_element_to_be_present(DATE_FILTER_DROPDOWN)
 
-        nynews_obj.click_on_btn("css:button.css-p5555t")
-        nynews_obj.wait_for_element_to_be_present("css:div.css-91q1y8")
-
-        date_options = nynews_obj.browser_lib.find_elements('css:button.css-jba1a6')
+        # Select option to specify date ranges
+        date_options = nynews_obj.browser_lib.find_elements(DATE_FILTER_OPTION)
         date_options[-1].click()
 
-        specific_date_inputs = nynews_obj.browser_lib.find_elements("css:input.css-9wn7z1")
-        start_date = today_date.replace(day=1) - relativedelta(month=1)
+        specific_date_inputs = nynews_obj.browser_lib.find_elements(DATE_INPUT_RANGE)
+        start_date = today_date.replace(day=1) - relativedelta(months=normalize_months_to_watch(number_of_months))
 
         specific_date_inputs[0].send_keys(start_date.strftime(STANDARD_DATE_FORMAT))
         specific_date_inputs[1].send_keys(today_date.strftime(STANDARD_DATE_FORMAT))
 
-        nynews_obj.click_on_btn("css:button.css-p5555t.popup-visible.calendar-open")
+        nynews_obj.click_on_btn(HIDE_DATE_FILTER_BTN)
 
-        # title_elements = nynews_obj.browser_lib.find_elements("css:h4.css-2fgx4k")
-        # date_elements = nynews_obj.browser_lib.find_elements("css:span.css-17ubb9w")
-        # description_elements = nynews_obj.browser_lib.find_elements("css:p.css-16nhkrn")
+        # Create a directory of the execution results with the today's datetime as name
+        os.makedirs(f"{destination_folder}/images")
 
-        news_elements = nynews_obj.browser_lib.find_elements("css:li.css-1l4w6pd")
+        while nynews_obj.browser_lib.is_element_visible(SHOW_MORE_BTN):
+            nynews_obj.click_on_btn(SHOW_MORE_BTN)
+            # TODO
+            # This delay is neccesary when button appears more than 1 time
+            # But soon the page will be so large that will last more to load this button
+            time.sleep(1)
+
+        news_elements = nynews_obj.browser_lib.find_elements(NEWS_ELEMENT)
         for news_element in news_elements:
-            title_element = nynews_obj.browser_lib.find_element("css:h4.css-2fgx4k", news_element).text
-            date_element = nynews_obj.browser_lib.find_element("css:span.css-17ubb9w", news_element).text
-            description_element = nynews_obj.browser_lib.find_element("css:p.css-16nhkrn", news_element).text
+            # Stores the news data in this list in order: title, date, description, filename
+            tmp_new_obj = []
+            title_element = nynews_obj.browser_lib.find_element(TITLE_NEWS_ELEMENT,
+                                                                news_element).text
+            date_element = nynews_obj.browser_lib.find_element(DATE_NEWS_ELEMENT,
+                                                               news_element).text
+            description_element = nynews_obj.browser_lib.find_element(DESCRIPTION_NEWS_ELEMENT,
+                                                                      news_element).text
+            tmp_new_obj.append(title_element)
+            tmp_new_obj.append(date_element)
+            tmp_new_obj.append(description_element)
+
+            # Check if title or description contains any amount of money
+            if find_dollar_ocurrence(title_element) or find_dollar_ocurrence(description_element):
+                tmp_new_obj.append("True")
+            else:
+                tmp_new_obj.append("False")
             try:
-                image = nynews_obj.browser_lib.find_element("css:img.css-rq4mmj", news_element)
+                # In case the image of the new doesn't exists
+                image = nynews_obj.browser_lib.find_element(IMAGE_NEWS_ELEMENT,
+                                                            news_element)
                 image_url = image.get_attribute("src")
+
+                # Filtering url and query params to get the filename
                 image_filename = image_url.split("/")[-1].split("?")[0]
-                # with open(f"logs/{today_date.strftime(EXECUTION_NAME_FORMAT)}/images/{image_filename}", "wb") as file:
-                with open(image_filename, "wb") as file:
+                with open(f"{destination_folder}/images/{image_filename}", "wb") as file:
                     image_response = requests.get(image_url, stream=True)
-                    for image_batch in image_response.iter_content(1024):
+
+                    # Since images are large size, we download them by chunks
+                    for image_batch in image_response.iter_content(1 * KILOBYTE):
                         if not image_batch:
                             break
                         file.write(image_batch)
+                tmp_new_obj.append(image_filename)
             except ElementNotFound as e:
-                print("not found")
-
-        time.sleep(15)
+                tmp_new_obj.append(NOT_FOUND_ELEMENT)
+            finally:
+                # In case of failures on getting information, the news information will be kept
+                df_rows_list.append(tmp_new_obj)
 
     finally:
+        # In case the webpage fails at the middle of the script, it will save the data obtained
         nynews_obj.quit_browsers()
 
+        df_data = pandas.DataFrame(df_rows_list)
+        df_data.to_excel(f"{destination_folder}/data.xlsx", header=OUTPUT_HEADERS, index=False)
 
-# Call the main() function, checking that we are running as a stand-alone script:
+
 if __name__ == "__main__":
-    main()
+    word = "murder"
+    categories = ["Arts"]#, "U.S.", "World"]
+    number_of_months = 0
+    main(word, categories, number_of_months)
